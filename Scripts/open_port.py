f370976058e3677ca47d69456aa1e9728f3451bc
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-20 15:45:46 -0700

    opening 4 ports

--- Scripts/open_port.py
+++ Scripts/open_port.py
@@ -5,27 +5,43 @@
 
 import socket
 import random
-import _thread
+import threading
+
+class portThread(threading.Thread):
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.port = port
+
+    def run(self):
+        print("Opening port {}".format(self.port))
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('127.0.0.1', self.port))
+        s.listen(1)
+        while True:
+            print("Listening on port", self.port)
+            c, addr=s.accept()
+            print("Connection from", addr)
 
 def generate_port():
     return(random.randrange(10000, 20000))
 
-def open_port(port_number):
-    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', port_number))
-    s.listen(1)
-    while True:
-        print("Listening on port", port_number)
-        c, addr=s.accept()
-        print("Connection from", addr)
-        print("Closing")
-        # s.close()
-        # break
-    s.exit()
+# def open_port(port_number):
+#     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     s.bind(('0.0.0.0', port_number))
+#     s.listen(1)
+#     while True:
+#         print("Listening on port", port_number)
+#         c, addr=s.accept()
+#         print("Connection from", addr)
+#         print("Closing")
+#         # s.close()
+#         # break
+#     s.exit()
 
 if __name__=="__main__":
     ports = [generate_port() for i in range(4)]
-    print(ports)
+    # print(ports)
     for port in ports:
-        _thread.start_new_thread(open_port, (port,))
+        # _thread.start_new_thread(open_port, (port,))
+        portThread(port).start()
     # open_port(10000)
