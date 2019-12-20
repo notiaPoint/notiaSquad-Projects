367ed9637de97a673378c5893edc5c0e922990df
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-20 16:30:11 -0700

    updated port range

--- Scripts/open_port.py
+++ Scripts/open_port.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-# Guilhem Mizrahi 09/2019
-# this program opens a specific port
+# Guilhem Mizrahi 12/2019
+# this program opens 4 random ports in a specified range
 
 import socket
 import random
@@ -15,7 +15,7 @@ class portThread(threading.Thread):
     def run(self):
         print("Opening port {}".format(self.port))
         s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('127.0.0.1', self.port))
+        s.bind(('', self.port))
         s.listen(1)
         while True:
             print("Listening on port", self.port)
@@ -23,25 +23,9 @@ class portThread(threading.Thread):
             print("Connection from", addr)
 
 def generate_port():
-    return(random.randrange(10000, 20000))
-
-# def open_port(port_number):
-#     s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     s.bind(('0.0.0.0', port_number))
-#     s.listen(1)
-#     while True:
-#         print("Listening on port", port_number)
-#         c, addr=s.accept()
-#         print("Connection from", addr)
-#         print("Closing")
-#         # s.close()
-#         # break
-#     s.exit()
+    return(random.randrange(1024, 2048))
 
 if __name__=="__main__":
     ports = [generate_port() for i in range(4)]
-    # print(ports)
     for port in ports:
-        # _thread.start_new_thread(open_port, (port,))
         portThread(port).start()
-    # open_port(10000)
