a2c0f5c9903652479b5b2e835d6bd6271caadc92
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-20 16:48:42 -0700

    updated range

--- Scripts/ssh_config.py
+++ Scripts/ssh_config.py
@@ -1,24 +1,34 @@
 #!/usr/bin/python3
 
+# Guilhem Mizrahi 12/2019
+# Change the port for ssh and asign it to a random port in the range 1024-2048
+
 import random
 import subprocess
 import os
 
 def generate_port():
-    return(random.randrange(10000, 20000))
+    """
+    Return a random integer in the range 1024-2048. This value will be assigned to SSH
+    """
+    return(random.randrange(1024, 2048))
 
 def change_ssh_config(port):
-    #subprocess.run(["sed", "-i", "/Port/c\\Port "+str(port), "/etc/ssh/ssh_config"])
+    """
+    Change the port for SSH
+    """
     subprocess.run(["sed", "-i", "/Port/c\\Port "+str(port), "/etc/ssh/sshd_config"])
-    #subprocess.run(["systemctl", "restart", "sshd"])
 
 def main():
+    """
+    Check if user is root
+    """
     if os.geteuid()==0:
         port = generate_port()
         change_ssh_config(port)
         subprocess.run(["systemctl", "restart", "sshd"])
     else:
-        print("Permission denied.")
+        raise PermissionError("Permission denied for user with UID {}".format(os.geteuid()))
 
 if __name__=="__main__":
     main()
