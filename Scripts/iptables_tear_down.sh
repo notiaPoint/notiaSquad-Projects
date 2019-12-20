a06af51b41a954aa87f7c3082f385ff54b5183ce
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-20 16:48:16 -0700

    added permission handling

--- Scripts/iptables_tear_down.sh
+++ Scripts/iptables_tear_down.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # Restore the iptables rules to a fresh file allowing every connection
-iptables-restore < /etc/iptables/rules.v4
+
+function reset_iptables(){
+	if [[ $EUID -ne 0 ]]
+	then
+		echo "Permission denied. Script must be run as root";
+		exit 1;
+	else
+        iptables-restore < rules.v4;
+		return 0;
+	fi
+}
+
+reset_iptables;
