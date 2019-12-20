967d91408fad4036d8b46c39b761a51fab15255b
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-20 15:34:39 -0700

    threads to open multiple ports

---
+++ Scripts/open_port.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+# Guilhem Mizrahi 09/2019
+# this program opens a specific port
+
+import socket
+import random
+import _thread
+
+def generate_port():
+    return(random.randrange(10000, 20000))
+
+def open_port(port_number):
+    s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port_number))
+    s.listen(1)
+    while True:
+        print("Listening on port", port_number)
+        c, addr=s.accept()
+        print("Connection from", addr)
+        print("Closing")
+        # s.close()
+        # break
+    s.exit()
+
+if __name__=="__main__":
+    ports = [generate_port() for i in range(4)]
+    print(ports)
+    for port in ports:
+        _thread.start_new_thread(open_port, (port,))
+    # open_port(10000)
