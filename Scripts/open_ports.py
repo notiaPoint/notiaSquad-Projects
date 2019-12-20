e7fd8f59a5346b0af9a763a07fd5f28e29c81fc9
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-20 16:38:17 -0700

    updated name

---
+++ Scripts/open_ports.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Guilhem Mizrahi 12/2019
+# this program opens 4 random ports in a specified range
+
+import socket
+import random
+import threading
+
+class portThread(threading.Thread):
+    def __init__(self, port):
+        threading.Thread.__init__(self)
+        self.port = port
+
+    def run(self):
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', self.port))
+        s.listen(1)
+        while True:
+            print("Listening on port", self.port)
+            c, addr=s.accept()
+            print("Connection from", addr)
+
+def generate_port():
+    return(random.randrange(1024, 2048))
+
+if __name__=="__main__":
+    ports = [generate_port() for i in range(5)]
+    for port in ports:
+        portThread(port).start()
