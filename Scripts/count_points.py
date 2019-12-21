768253f276c2ee03acea16d03dbafe4a61a6b2cb
MIZRAHI Guilhem <guilhem.mizrahi@gmail.com>
2019-12-21 14:28:25 -0700

    removed comment

--- Scripts/count_points.py
+++ Scripts/count_points.py
@@ -13,14 +13,13 @@ def ping_list(ip_list, point_list):
     """
     while True:
         for i in range(len(ip_list)):
-            # res = os.system("ping -c1 "+ip_list[i]+" 2>&1 >/dev/null")
             res = subprocess.call("ping"+" -c1"+" -t2 "+ip_list[i]+" 2>&1 >/dev/null", shell=True)
             if res == 0:
                 point_list[i] += 1
             else:
                 print(ip_list[i], "is down")
         print([(ip_list[i], point_list[i]) for i in range(len(ip_list))])
-        # time.sleep(5)
+        time.sleep(5)
 
 def main(ip_list):
     point_list = [0]*len(ip_list)
